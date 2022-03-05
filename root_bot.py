e3ed3bbff640a840f7acd2da2c4fcbc33fe2bb6a
PyroFourTwenty <kermitthecreeper@gmail.com>
2022-03-05 13:33:19 +0000

    added semaphores

--- root_bot.py
+++ root_bot.py
@@ -9,11 +9,14 @@ from time import sleep, time
 from bs4 import BeautifulSoup
 from pathlib import Path
 import validators
+import asyncio
 
 TOKEN = str(open("../data.txt", "r").read())
 
 client = discord.Client()
 stream_requests_channel = "stream-requests"
+
+semaphore = asyncio.Semaphore()
 
 
 def add_to_soundcloud_playlist(url):
@@ -94,7 +97,12 @@ def remove_download_flag_from_message(message):
 
 @client.event
 async def on_message(message):
+    global semaphore
+    await semaphore.acquire()
+    print("semaphore acquired")
     if message.author == client.user:
+        semaphore.release()
+        print("semaphore released")
         return
     if str(message.channel).strip() == stream_requests_channel:
         download_requested = message.content.strip().endswith(" -download")
@@ -109,10 +117,14 @@ async def on_message(message):
             track_title, is_soundcloud_link, is_playlist = get_track_data(link)
             if track_title==r"SoundCloud - Hear the world’s sounds":
                 await message.channel.send("This track doesnt exist")
+                semaphore.release()
+                print("semaphore released")
                 return
             if is_soundcloud_link:
                 if is_playlist:
                     await message.channel.send("Sorry, but adding a playlist to a playlist doesnt really make much sense, does it?")
+                    semaphore.release()
+                    print("semaphore released")                
                     return
                 if not download_only:
                     await message.channel.send("Now adding "+str(track_title))
@@ -123,6 +135,8 @@ async def on_message(message):
                         print("[EXCEPTION] occured while downloading",link, "| stacktrace:")
                         print(e)
                         await message.channel.send('Sorry mate, something went wrong. Tell Pyro420 and he will try to find out what happened.')
+                        semaphore.release()
+                        print("semaphore released")
                         return
                     timestamp2 = time()
                     if result == "ADD_SUCCESS":
@@ -156,6 +170,8 @@ async def on_message(message):
         else:
             #await message.channel.send("Not a url "+link)
             pass
+    print("semaphore released")
+    semaphore.release()
         
 
 if __name__ == '__main__':
