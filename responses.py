2bddf6fc4376a2700a8b500b10f9d7fbb88885ad
PyroFourTwenty <kermitthecreeper@gmail.com>
2022-03-06 23:53:20 +0000

    fixed typo

---
+++ responses.py
@@ -0,0 +1,30 @@
+help = """
+Current command list: 
+
+- Add a soundcloud track to the playlist by simply posting the link in the #stream-requests channel
+Example usage: 
+```<soundcloud-link>```
+- Add a soundcloud track to the playlist and get the downloaded file sent here
+Example usage: 
+```<soundcloud-link> -download```
+- Just download the track and get it sent here
+Example usage: 
+```<soundcloud-link> -downloadonly```
+
+The playlist all these tracks are added to is here: https://soundcloud.com/luce-raspe/sets/root-dnbs-stream-requests
+
+Made by P̷̍̍y̴̓̾r̴͐͘o̶̊͒ ̷̈͂4̶̏̚2̴̰͗0̸͛̕
+"""
+
+individual_responses = {
+    183680794286489600 :{
+        "name": "DrHappybone"
+        "responses": [
+            """Well, well, well, if it isnt the infamous DrHappybone""",
+            """zzzZZZzzz... :sleeping: huh what? I thought i could take a nap... guess not. Thanks :rolling_eyes: okay back to work. :mechanical_arm::robot:""",
+            """You know what ... because its YOU doc, im gonna take my time and add the track EVEN slower""",
+            """Fun fact: bones cant be happy because they arent thinking beings. :nerd: Remember to get enough calcium to keep your bones strong and healthy tho :bone:""",
+            """Big up the Notorious D.O.C. :mechanical_arm:"""
+        ]
+    }
+}
