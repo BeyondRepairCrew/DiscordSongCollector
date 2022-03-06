82ba54db83f29e09a1ae464f451e74ef375e6fe2
PyroFourTwenty <kermitthecreeper@gmail.com>
2022-03-06 23:39:09 +0000

    added individual responds

--- responds.py
+++ responds.py
@@ -15,3 +15,16 @@ The playlist all these tracks are added to is here: https://soundcloud.com/luce-
 
 Made by P̷̍̍y̴̓̾r̴͐͘o̶̊͒ ̷̈͂4̶̏̚2̴̰͗0̸͛̕
 """
+
+individual_responds = {
+    183680794286489600 :{
+        "name": "DrHappybone"
+        "responds": [
+            """Well, well, well, if it isnt the infamous DrHappybone""",
+            """zzzZZZzzz... :sleeping: huh what? I thought i could take a nap... guess not. Thanks :rolling_eyes: okay back to work. :mechanical_arm::robot:""",
+            """You know what ... because its YOU doc, im gonna take my time and add the track EVEN slower""",
+            """Fun fact: bones cant be happy because they arent thinking beings. :nerd: Remember to get enough calcium to keep your bones strong and healthy tho :bone:""",
+            """Big up the Notorious D.O.C. :mechanical_arm:"""
+        ]
+    }
+}
